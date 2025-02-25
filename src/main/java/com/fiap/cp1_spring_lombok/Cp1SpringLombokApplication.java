75396a848d7c66dbbf694917bad87b382fa82386
Barros263inf <barros263inf@gmail.com>
2025-02-25 00:48:32 -0300

    Init Project

---
+++ src/main/java/com/fiap/cp1_spring_lombok/Cp1SpringLombokApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.cp1_spring_lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cp1SpringLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cp1SpringLombokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/cp1_spring_lombok/Cp1SpringLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.cp1_spring_lombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cp1SpringLombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
